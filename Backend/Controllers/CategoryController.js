26699419222748200823e416ea5ef2c99dced8a1
mugishab2020 <mugishab2020@gmail.com>
2025-01-29 11:15:20 +0200

    Making the get , create routes for category

--- Backend/Controllers/CategoryController.js
+++ Backend/Controllers/CategoryController.js
@@ -1,5 +1,4 @@
-import Category from '../Models/category.js';
-
+import Category from "../Models/category.js";
 
 export const createCategory = async (req, res) => {
   try {
@@ -7,7 +6,9 @@ export const createCategory = async (req, res) => {
 
     const existingCategory = await Category.findOne({ name });
     if (existingCategory) {
-      return res.status(400).json({ message: 'Category with this name already exists.' });
+      return res
+        .status(400)
+        .json({ message: "Category with this name already exists." });
     }
 
     const newCategory = new Category({
@@ -17,35 +18,46 @@ export const createCategory = async (req, res) => {
     await newCategory.save();
 
     res.status(201).json({
-      message: 'Category created successfully!',
+      message: "Category created successfully!",
       category: newCategory,
     });
   } catch (error) {
     res.status(500).json({
-      message: 'Error creating category',
+      message: "Error creating category",
       error: error.message,
     });
   }
 };
 
+export const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching categories",
+      error: error.message,
+    });
+  }
+};
 export const deleteCategory = async (req, res) => {
   try {
     const { categoryId } = req.params;
 
-    
     const category = await Category.findById(categoryId);
     if (!category) {
-      return res.status(404).json({ message: 'Category not found.' });
+      return res.status(404).json({ message: "Category not found." });
     }
 
     await category.remove();
 
     res.status(200).json({
-      message: 'Category deleted successfully!',
+      message: "Category deleted successfully!",
     });
   } catch (error) {
     res.status(500).json({
-      message: 'Error in deleting category',
+      message: "Error in deleting category",
       error: error.message,
     });
   }
--- Backend/Routes/CategoryRoutes.js
+++ Backend/Routes/CategoryRoutes.js
@@ -1,9 +1,13 @@
-import express from 'express';
-import { createCategory, deleteCategory } from '../Controllers/CategoryController.js'; 
+import express from "express";
+import {
+  createCategory,
+  deleteCategory,
+} from "../Controllers/CategoryController.js";
 
 const categoryRoutes = express.Router();
 
-categoryRoutes.post('/', createCategory);
-categoryRoutes.delete('/:categoryId', deleteCategory);
+categoryRoutes.post("/createCategory", createCategory);
+categoryRoutes.delete("/deleteCategory/:categoryId", deleteCategory);
+categoryRoutes.get("/", getCategories);
 
 export default categoryRoutes;
