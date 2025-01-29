e19d4c4b78cd17aefc2ae8acda16a1070e25b232
mugishab2020 <mugishab2020@gmail.com>
2025-01-29 09:20:56 +0200

    Updating the transaction fetching logic

--- Backend/Controllers/TransactionController.js
+++ Backend/Controllers/TransactionController.js
@@ -65,7 +65,7 @@ export const getTransactionsByAccountId = async (req, res) => {
     const { account_id } = req.params;
 
     const transactions = await Transaction.find({
-      account_id,
+      account_id: account_id,
     });
 
     res.status(200).json(transactions);
